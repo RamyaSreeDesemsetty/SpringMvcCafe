1bdab4f40e1406fe3e513783e8c98f099fbf705e
RamyaSreeDesemsetty <ramyadesemsetty@gmail.com>
2024-11-13 22:19:16 +0530

    changes made

---
+++ src/main/java/com/contollercafe/CafeController.java
@@ -0,0 +1,24 @@
+package com.contollercafe;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CafeController {
+	@RequestMapping("/cafehome")
+	public String getCafe(Model model) {
+		String name = "Ramya";
+		model.addAttribute("myname", name);
+		return "welcome-cafe";
+	}
+
+	@RequestMapping("/processOrder")
+	public String getOrder(HttpServletRequest request, Model model) {
+		String order_name = request.getParameter("itemName");
+		model.addAttribute("items", order_name);
+		return "order";
+	}
+}
